747706726abc3896cd97fa645669c5ca4b356fac
forum010 <vastekhealthcare@gmail.com>
2025-05-04 11:21:07 +0530

    fix: use correct type definition for theme provider props

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,15 @@
 "use client"
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
+type ThemeProviderProps = {
+  children: React.ReactNode
+  attribute?: "class" | "data-theme"
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
